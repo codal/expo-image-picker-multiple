595859034d50dcbc5a7abebc550fb2e81843c2e6
Manthan Pawar <46340921+codal-mpawar@users.noreply.github.com>
2022-06-01 10:28:33 +0530

    Update ImageTile.js

--- src/ImageTile.js
+++ src/ImageTile.js
@@ -4,17 +4,29 @@ import {
   ImageBackground,
   TouchableHighlight,
   View,
+  Text
 } from 'react-native';
 
 const {width} = Dimensions.get('window');
-
+const renderTime = (value) => {
+    const sec = parseInt(value, 10); // convert value to number if it's string
+      let hours   = Math.floor(sec / 3600); // get hours
+      let minutes = Math.floor((sec - (hours * 3600)) / 60); // get minutes
+      let seconds = sec - (hours * 3600) - (minutes * 60); //  get seconds
+      // add 0 if value < 10; Example: 2 => 02
+      if (hours   < 10) {hours   = "0"+hours;}
+      if (minutes < 10) {minutes = "0"+minutes;}
+      if (seconds < 10) {seconds = "0"+seconds;}
+  
+    return hours > 0 ? `${hours}:${minutes}:${seconds}` : `${minutes}:${seconds}`
+  }
 class ImageTile extends React.PureComponent {
   render() {
     const { item, index, selected, selectImage, selectedItemNumber, renderSelectedComponent, renderExtraComponent } = this.props;
     if (!item) return null;
+    const { mediaType, duration = '' } = item
     return (
       <TouchableHighlight
-        style={{ opacity: selected ? 0.5 : 1 }}
         underlayColor='transparent'
         onPress={() => selectImage(index)} >
         <View style={{ position: 'relative' }}>
@@ -25,6 +37,12 @@ class ImageTile extends React.PureComponent {
               {selected && renderSelectedComponent && renderSelectedComponent(selectedItemNumber)}
               {renderExtraComponent && renderExtraComponent(item)}
             </ImageBackground>
+            {!selected && mediaType == 'video' && (
+              <View style={{position: 'absolute', right: 5,bottom: 5, zIndex: 1}}>
+              <Text style={{color: '#fff', fontSize: 14, fontWeight: '600'}}>{renderTime(duration)}</Text>
+              </View>
+            )}
+            {selected && renderSelectedComponent && renderSelectedComponent(selectedItemNumber)}
           </View>
         </View>
       </TouchableHighlight>
